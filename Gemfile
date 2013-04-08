1cd717e23167cd23ac65c04b9b8da65a13192858
Chris Your <chris@ignitionindustries.com>
2013-04-08 10:38:48 -0600

    Initial commit.

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source 'https://rubygems.org'
+
+# Specify your gem's dependencies in gravatar-rails.gemspec
+gemspec
---
+++ lib/gravatar/rails.rb
@@ -0,0 +1,7 @@
+require "gravatar/rails/version"
+
+module Gravatar
+  module Rails
+    # Your code goes here...
+  end
+end
